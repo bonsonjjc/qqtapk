e51747fce6bc2160f6d0924b0e559ee91389676a
bonsonjjc <bonsonjjc@yahoo.com>
2018-01-08 01:24:47 +0800

    first commit

--- app/src/main/java/com/bonson/qqtapk/view/ui/ring/RingViewModel.java
+++ app/src/main/java/com/bonson/qqtapk/view/ui/ring/RingViewModel.java
@@ -2,14 +2,22 @@ package com.bonson.qqtapk.view.ui.ring;
 
 import android.app.Application;
 
+import com.bonson.qqtapk.model.data.ring.RingModel;
 import com.bonson.resource.viewmodel.AndroidViewModel;
+
+import javax.inject.Inject;
 
 /**
  * Created by jiangjiancheng on 18/1/8.
  */
 
 public class RingViewModel extends AndroidViewModel {
-    public RingViewModel(Application application) {
+    private RingModel ringModel;
+
+    @Inject
+
+    public RingViewModel(Application application, RingModel ringModel) {
         super(application);
+        this.ringModel = ringModel;
     }
 }
